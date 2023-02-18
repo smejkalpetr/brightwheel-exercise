582ab59928304f06cef1ae2631ebeddc79307062
Petr Smejkal <petr.smejkal@matee.cz>
2023-02-18 10:53:51 +0100

    Initial Commit

---
+++ Brightwheel/BrightwheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrightwheelApp.swift
+//  Brightwheel
+//
+//  Created by Petr Šmejkal on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct BrightwheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
